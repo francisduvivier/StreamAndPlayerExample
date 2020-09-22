14dbae349ee4bc258a4b7f074d686d430c53c02a
Francis Duvivier <francisduvivier@gmail.com>
2020-09-22 09:19:24 +0200

    Make the stream address configurable via gradle property
    This allows it to be passed with -pstreamAddress "http://..." to the gradle command

--- android/app/src/main/java/io/github/francisduvivier/simpleplayer/VideoSources.java
+++ android/app/src/main/java/io/github/francisduvivier/simpleplayer/VideoSources.java
@@ -1,9 +1,8 @@
 package io.github.francisduvivier.simpleplayer;
 
 public class VideoSources {
-    private static final String serverHost = "192.168.0.197:8080";
     static final VideoSourceData[] sources = new VideoSourceData[]{
             new VideoSourceData("VOD Test Stream", "https://storage.googleapis.com/wvmedia/clear/h264/tears/tears.mpd"),
-            new VideoSourceData("Live Test Stream", "http://"+serverHost+"/stream1/manifest.m3u8")
+            new VideoSourceData("Live Test Stream", BuildConfig.LIVE_STREAM_ADDRESS)
     };
 }
